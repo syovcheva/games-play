7f94f377832f4492e469a729ceb676eaf12474e2
syovcheva <s.yovcheva03@gmail.com>
2023-12-03 10:27:22 +0200

    setup React router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import Header from "./components/header/Header"
-
+import Home from "./components/home/Home"
+import { Route, Routes } from "react-router-dom"
 function App() {
 
   return (
     <div id="box">
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+
+
     </div>
   )
 }
